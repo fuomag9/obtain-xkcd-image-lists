902b5be209f17957c37022e09b4fbc4ef072bba4
fuomag9 <fuomag9@hotmail.it>
2017-04-29 19:46:56 +0200

    removed print spam during the image list obtaining

--- obtainxkcd.py
+++ obtainxkcd.py
@@ -16,29 +16,31 @@ elenco_siti=[]
 elenco_siti_corretto=[]
 immagini=[]
 immagini_corrette=[]
-file=os.getcwd()+"/pls.txt"
+file=os.getcwd()+"/list.txt"
 for link in get_html("http://xkcd.com/archive/").find_all('a'):
     elenco_siti.append(link.get('href'))
 for x in range(0,len(elenco_siti)):
     if "." not in elenco_siti[x]:
         elenco_siti_corretto.append(elenco_siti[x])
-print(elenco_siti_corretto)
-for x in range(3,len(elenco_siti_corretto)):
- try:
-    for link in get_html("http://xkcd.com"+elenco_siti_corretto[x]).find_all('img'):
+i=0
+try:
+ for x in range(3,len(elenco_siti_corretto)):
+  for link in get_html("http://xkcd.com"+elenco_siti_corretto[x]).find_all('img'):
      immagini.append(link.get('src'))
-     print(str(x)+"/"+str(len(elenco_siti_corretto)))
- except urllib.error.HTTPError:
-        pass 
+     if (i==0):
+      print(str(x)+"/"+str(len(elenco_siti_corretto)))
+      i=i+2
+     else:
+      i=i-1
+except urllib.error.HTTPError:
+        pass
 for x in range(0,len(immagini)):
     if "/s/" not in immagini[x]:
         immagini_corrette.append(immagini[x])
-        print(str(x)+"/"+str(len(immagini)))
 immagini=[]
 for x in range(0,len(immagini_corrette)):
     if "static" not in immagini_corrette[x]:
         immagini.append(immagini_corrette[x])
-        print(str(x)+"/"+str(len(immagini_corrette)))
 with open(file, 'w') as f:
     for s in immagini:
         f.write("http:"+s + '\n')
